4e69ad6721fda97c6ea7e7b086234691265613d7
Col-R <2colewestin@gmail.com>
2021-12-01 19:05:10 -0600

    added getters and setters for new ninja and created at/updated at

--- src/main/java/com/cole/dojosandninjas/models/Dojo.java
+++ src/main/java/com/cole/dojosandninjas/models/Dojo.java
@@ -1,12 +1,16 @@
 package com.cole.dojosandninjas.models;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -28,6 +32,9 @@ public class Dojo {
 	private Date createdAt;
 	@DateTimeFormat(pattern="yyy-MM-DD HH:mm:ss")
 	private Date updatedAt;
+	
+	@OneToMany(mappedBy = "dojoLocation", cascade = CascadeType.ALL, fetch=FetchType.LAZY)
+	private List<Ninja> ninjas;
 	
 	@PrePersist
 	protected void onCreate() {
@@ -56,6 +63,24 @@ public class Dojo {
 	}
 	public void setDojoName(String dojoName) {
 		this.dojoName = dojoName;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public List<Ninja> getNinjas() {
+		return ninjas;
+	}
+	public void setNinjas(List<Ninja> ninjas) {
+		this.ninjas = ninjas;
 	}
 	
 	

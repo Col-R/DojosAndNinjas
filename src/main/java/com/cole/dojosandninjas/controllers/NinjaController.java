538fe67645e68846debd27b9afc5a3bea16ef276
Col-R <2colewestin@gmail.com>
2021-12-01 18:45:44 -0600

    added get mapping for ninja controller

--- src/main/java/com/cole/dojosandninjas/controllers/NinjaController.java
+++ src/main/java/com/cole/dojosandninjas/controllers/NinjaController.java
@@ -2,8 +2,12 @@ package com.cole.dojosandninjas.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cole.dojosandninjas.models.Ninja;
 import com.cole.dojosandninjas.services.DojoService;
 import com.cole.dojosandninjas.services.NinjaService;
 
@@ -14,4 +18,10 @@ public class NinjaController {
 	NinjaService nService;
 	@Autowired
 	private DojoService dService;
+	
+	@GetMapping("/new")
+	public String create(@ModelAttribute("ninja")Ninja ninja, Model viewModel) {
+		viewModel.addAttribute("dojos", this.dService.getAllDojos());
+		return"/ninjas/ninjaadd.jsp";
+	}
 }
